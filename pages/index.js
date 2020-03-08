16be29e6abce2ea640a03e37072e6cd644647998
Andrew Kachnic <ajkachnic@protonmail.com>
2020-03-08 07:59:12 -0400

    New Feature: I added a fallback font families
    I added a new fallback font families for when it takes awhile to load

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const mainTheme = {
     }
   },
   fontFamily: {
-    primary: "Inter",
+    primary: 'Inter, -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
     secondary:"sans-serif"
   },
   colors: {
