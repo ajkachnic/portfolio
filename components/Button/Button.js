70e5bffa15f97fee28a061f6324c071878534085
Andrew Kachnic <ajkachnic@protonmail.com>
2020-03-08 07:45:38 -0400

    Updated Various Components
    I updated the styling of the button and the card

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -8,7 +8,7 @@ const NewButton = (props) => {
 	return (
 		<Button bg="highlight" hoverBg="info700" m={{y:"2rem"}} rounded="sm"
 		onClick={() => scrollTo(props.scroll)} 
-		id={props.id} textColor="white" textSize="heading" textWeight="500" shadow="1" hoverShadow="2">
+		id={props.id} textColor="white" textSize="heading" textWeight="500" shadow="2" hoverShadow="4" p="2rem">
 			{props.text}
 		</Button>
 	)
