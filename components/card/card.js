da63b8ae47de3ada1181b43c32f120c1d90f5e4c
Andrew Kachnic <ajkachnic@protonmail.com>
2020-05-24 17:36:35 -0400

    Updated Errors in the Card

--- components/card/card.js
+++ components/card/card.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../provider/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../provider/ThemeContext";
 
-const Heading = ({
-  body, title, link, linkText,
-}) => {
+const Card = ({ body, title, link, linkText }) => {
   const theme = useContext(ThemeContext);
 
   return (
@@ -11,9 +9,9 @@ const Heading = ({
       <style jsx>
         {`
           div.card {
-              box-shadow: 0px 5px 5px rgba(0, 0, 0, .15);
-              padding: 2rem;
-              background: ${theme.colors.cardBackground}
+            box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.15);
+            padding: 2rem;
+            background: ${theme.colors.cardBackground};
           }
           h3.card__title {
             color: ${theme.colors.foreground};
@@ -21,25 +19,29 @@ const Heading = ({
             font-weight: 900;
             margin: 0 0 1rem;
           }
-          p.card__body{
-              font-size:1.25rem;
+          p.card__body {
+            font-size: 1.25rem;
           }
 
           @media (max-width: 1200px) {
-           
           }
           @media (max-width: 800px) {
-           
           }
         `}
       </style>
       <div className="card">
-        {title ? <h3 className="card__title">{title}</h3> : ''}
-        {body ? <p className="card__body">{body}</p> : ''}
-        {title ? <a className="card__link" href={link}>{linkText}</a> : ''}
+        {title ? <h3 className="card__title">{title}</h3> : ""}
+        {body ? <p className="card__body">{body}</p> : ""}
+        {link ? (
+          <a className="card__link" href={link}>
+            {linkText}
+          </a>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
 };
 
-export default Heading;
+export default Card;
